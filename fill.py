fbf1f349027d08ca2ed6c1dd75c201f1ee95e573
kasa10 <kirill.kas@mail.ru>
2022-06-29 15:43:37 +0300

    v1.1

---
+++ fill.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib
+#%%
+from sklearn.model_selection import train_test_split
+df=pd.read_csv('data/train.csv')
+df2=pd.read_csv('data/interpolate.csv')
+
+#%%
+pd.isnull(df)
+
+#%% Количество пропущенных значений
+pd.isnull(df).sum()
+
+#%%
+x=0
+if df['Номер скважины'] == 41:
+    x=x+1
+
